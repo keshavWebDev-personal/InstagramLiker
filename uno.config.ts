75844ef4879adfb3c77bf0008b90164c3e36b249
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-07-30 13:34:54 +0530

    added grouping, font Uno CSS Libraries

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,31 @@
-// uno.config.ts
 import { defineConfig } from 'unocss'
+import presetWebFonts from '@unocss/preset-web-fonts'
+import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
+
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetWebFonts({
+      provider: 'none',
+      fonts: {
+        sans: 'Inter'
+      },
+      // This will download the fonts and serve them locally
+      processors: createLocalFontProcessor({
+        // Directory to cache the fonts
+        cacheDir: 'node_modules/.cache/unocss/fonts',
+
+        // Directory to save the fonts assets
+        fontAssetsDir: 'public/assets/fonts/',
+
+        // Base URL to serve the fonts from the client
+        fontServeBaseUrl: '/assets/fonts/Inter-Regular.woff'
+      })
+    }),
+  ],
+  transformers: [
+    transformerVariantGroup(),
+  ],
+
 })
