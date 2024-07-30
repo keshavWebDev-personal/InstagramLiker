9aedc707ee605f6cae0b7e181d7307a711682596
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-07-30 22:24:19 +0530

    added remote google font

--- uno.config.ts
+++ uno.config.ts
@@ -1,31 +1,20 @@
+// uno.config.ts
 import { defineConfig } from 'unocss'
-import presetWebFonts from '@unocss/preset-web-fonts'
-import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local'
 import transformerVariantGroup from '@unocss/transformer-variant-group'
-
+import presetWebFonts from '@unocss/preset-web-fonts'
 
 export default defineConfig({
   presets: [
+    // presetUno(),
     presetWebFonts({
-      provider: 'none',
+      provider: 'google', // default provider
       fonts: {
-        sans: 'Inter'
+        // these will extend the default theme
+        sans: ['Inter'],
       },
-      // This will download the fonts and serve them locally
-      processors: createLocalFontProcessor({
-        // Directory to cache the fonts
-        cacheDir: 'node_modules/.cache/unocss/fonts',
-
-        // Directory to save the fonts assets
-        fontAssetsDir: 'public/assets/fonts/',
-
-        // Base URL to serve the fonts from the client
-        fontServeBaseUrl: '/assets/fonts/Inter-Regular.woff'
-      })
     }),
   ],
   transformers: [
     transformerVariantGroup(),
   ],
-
 })
