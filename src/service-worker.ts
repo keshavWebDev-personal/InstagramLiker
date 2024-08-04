02c39cdb0a68e6a40196275212357e26a451bcee
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-08-04 11:35:21 +0530

    build: use the seperate ts file for WebPageContext, change msg type

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,111 +1,11 @@
+import { webPageContext } from './.ts';
+
 let totalLikesCount = 0;
 let likesLimit = 0;
 
 // -------------------------------------------------
 // ------------------Some Functions-----------------
 // -------------------------------------------------
-
-
-function webPageContext() {
-    let randTime = 0;
-    let taskRunning = false;
-    let timeOutId: number | null = null;
-    let interId:number | null = null
-    let raf:number | null = null;
-
-    let getNextBtn = (): Promise<SVGElement> => {
-        return new Promise((resolve, reject) => {
-            let likeElem: SVGAElement | null = document.querySelector('svg[aria-label="Next"]');
-            if (likeElem) resolve(likeElem);
-            
-            let timeLimit = 2
-            let i = 0
-            let check = () => {
-                if (i > timeLimit * 60) reject("getNextBtn() :- "+ timeLimit + " secs have passed\n and 'next' element wasnt found,\n therefore :-");
-
-                likeElem = document.querySelector('svg[aria-label="Next"]');
-                if (!likeElem) {
-                    i++;
-                    raf = requestAnimationFrame(check);
-                } else resolve(likeElem);
-            };
-            check();
-        });
-    };
-    
-    let likeTaskRecursive = async (maxTime: number, minTime: number, likesLimit: number) => {
-        taskRunning = true;
-        let likeElem = document.querySelector(
-            'svg[aria-label="Like"][height="24"]'
-        );
-
-        if (likeElem){
-            likeElem.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-            chrome.runtime.sendMessage({
-                type: "data",
-                title: "Did a like",
-            });
-            randTime = Math.floor(Math.random() * (maxTime - minTime) + minTime);
-            console.log("randTime: " + randTime + "Min: " + minTime + "Max: " + maxTime);
-        }else{
-            randTime = 0
-        }
-
-        timeOutId = setTimeout(async ()=>{
-            try {
-                const nextBtn = await getNextBtn()
-                nextBtn.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-                likeTaskRecursive(maxTime, minTime, likesLimit)                
-            } catch (error) {
-                console.log(error);
-                chrome.runtime.sendMessage({
-                    type: "data",
-                    title: "reached end of page",
-                });
-                
-                taskRunning = false;
-            }
-        }, randTime);
-    }
-    chrome.runtime.onMessage.addListener(
-        ({ type, title, ...data }, _, sendResponse) => {
-            switch (type) {
-                case "action":
-                    switch (title) {
-                        case "Stop Likes Task":
-                            if (raf) cancelAnimationFrame(raf);
-                            if (interId) clearInterval(interId)
-                            if (timeOutId) clearTimeout(timeOutId);
-                            if (timeOutId || interId) {
-                                taskRunning = false;
-                                sendResponse({ status: true})
-                            }else{
-                                sendResponse({ status: false})
-                            }
-                            break;
-                        case "Start Liking":
-                            try {
-                                likeTaskRecursive(data.maxTime, data.minTime, data.likesLimit);
-                                sendResponse({ status: true})
-                            } catch (error) {
-                                sendResponse({ status: false})
-                            }
-                            break
-                    }
-                    break;
-                case "data":
-                    switch (title) {
-                        case "give me task status":
-                            sendResponse({
-                                taskRunning: taskRunning,
-                            });
-                            break;
-                    }
-                    break;
-            }
-        }
-    );
-};
 
 function stopAllLikeTasksLoops():Promise<Boolean> {
     return new Promise(async (resolve, reject) => {
@@ -217,7 +117,7 @@ chrome.runtime.onMessage.addListener(({ type, title, ...data }, _, sendResponse)
 
                         if (totalLikesCount >= likesLimit) {
                             stopAllLikeTasksLoops();
-                            chrome.runtime.sendMessage({ type: "data", title: "Target Like Reached" });
+                            chrome.runtime.sendMessage({ type: "info", title: "Target Like Reached" });
                         }
                         break;
                     case "give me likes count":
