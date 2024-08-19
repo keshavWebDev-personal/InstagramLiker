406055aaf853830909222b41537774bbf210d806
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-08-19 07:42:41 +0530

    fix:  setup of webpage context multiple times fix: Stop Like action response logic refactor: code formatting

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,7 +2,7 @@ import { webpageContext } from './webpageContext.ts';
 
 let totalLikesCount = 0;
 let likesLimit = 0;
-
+let setupTabs:chrome.tabs.Tab[] = []
 // -------------------------------------------------
 // ------------------Some Functions-----------------
 // -------------------------------------------------
@@ -15,14 +15,16 @@ function stopAllLikeTasksLoops():Promise<Boolean> {
 
             // Query for tabs matching these URL patterns
             let tabs = await chrome.tabs.query({ url: urlPatterns });
+            let success = false
             for (let tab of tabs) {
                 if (!tab.id) return;
                 let {status} = await chrome.tabs.sendMessage(tab.id, {
                     type: "action",
                     title: "Stop Likes Task",
                 });
-                resolve(status)
+                success = status || success
             }
+            resolve(success)
         }
     })
 
@@ -51,38 +53,39 @@ async function startLiking_currPage(
 // -------------------------------------------------
 
 chrome.runtime.onMessage.addListener(({ type, title, ...data }, _, sendResponse) => {
-        switch (type) {
-            case "action":
-                switch (title) {
-                    case "Start Liking":
-                        (async () => {
-                            try {
-                                let status = await startLiking_currPage(
-                                    data.maxTime,
-                                    data.minTime,
-                                );
-                                sendResponse({ status: status });    
-                            } catch (error) {
-                                sendResponse({ status: false });    
-                            }
-                        })()
-                        return true
-                        break;
-                    case "Stop Liking":
-                        (async () => {
-                            try {
-                                let status = await stopAllLikeTasksLoops();
-                                sendResponse({ status: status });    
-                            } catch (error) {
-                                sendResponse({ status: false });    
-                            }
-                        })()
-                        return true
-                        break;
-                    case "setup webpage context":
-                        (async () => {
-                            let tabs = await chrome.tabs.query({});
-                            tabs.forEach((tab) => {
+    switch (type) {
+        case "action":
+            switch (title) {
+                case "Start Liking":
+                    (async () => {
+                        try {
+                            let status = await startLiking_currPage(
+                                data.maxTime,
+                                data.minTime,
+                            );
+                            sendResponse({ status: status });    
+                        } catch (error) {
+                            sendResponse({ status: false });    
+                        }
+                    })()
+                    return true
+                    break;
+                case "Stop Liking":
+                    (async () => {
+                        try {
+                            let status = await stopAllLikeTasksLoops();
+                            sendResponse({ status: status });    
+                        } catch (error) {
+                            sendResponse({ status: false });    
+                        }
+                    })()
+                    return true
+                    break;
+                case "setup webpage context":
+                    (async () => {
+                        let tabs = await chrome.tabs.query({});
+                        tabs.forEach((tab) => {
+                            if (!setupTabs.includes(tab)){
                                 const {host_permissions} = chrome.runtime.getManifest();
                                 if (tab.id && tab.url?.match(host_permissions[0])){
                                     chrome.scripting.executeScript({
@@ -90,90 +93,91 @@ chrome.runtime.onMessage.addListener(({ type, title, ...data }, _, sendResponse)
                                         func: webpageContext,
                                     });
                                 }
-                            })
-                        })()
-                        break
-                }
-                break;
-            case "data":
-                switch (title) {
-                    case "Did a like":
-                        totalLikesCount++;
-                        chrome.runtime.sendMessage({
-                            type: "data",
-                            title: "Like Count",
-                            data: totalLikesCount,
-                        });
-                        // Store total likes count
-                        chrome.storage.sync.set({
-                            likesCount: {
-                                value: totalLikesCount,
-                                timestamp: Date.now(),
-                            },
-                        });
+                            }
+                        })
+                        setupTabs = tabs
+                    })()
+                    break
+            }
+            break;
+        case "data":
+            switch (title) {
+                case "Did a like":
+                    totalLikesCount++;
+                    chrome.runtime.sendMessage({
+                        type: "data",
+                        title: "Like Count",
+                        data: totalLikesCount,
+                    });
+                    // Store total likes count
+                    chrome.storage.sync.set({
+                        likesCount: {
+                            value: totalLikesCount,
+                            timestamp: Date.now(),
+                        },
+                    });
 
-                        if (totalLikesCount >= likesLimit) {
-                            stopAllLikeTasksLoops();
-                            chrome.runtime.sendMessage({ type: "info", title: "Target Like Reached" });
-                        }
-                        break;
-                    case "give me likes count":
-                        (async () => {
-                            try {
-                                let res = await chrome.storage.sync.get(["likesCount"]);
-                                if (!res.likesCount || res.likesCount.value === undefined) {
-                                    totalLikesCount = 0
+                    if (totalLikesCount >= likesLimit) {
+                        stopAllLikeTasksLoops();
+                        chrome.runtime.sendMessage({ type: "info", title: "Target Like Reached" });
+                    }
+                    break;
+                case "give me likes count":
+                    (async () => {
+                        try {
+                            let res = await chrome.storage.sync.get(["likesCount"]);
+                            if (!res.likesCount || res.likesCount.value === undefined) {
+                                totalLikesCount = 0
 
+                            }else{
+                                const now = new Date();
+                                const yesterday10PM = new Date(now);
+                                yesterday10PM.setDate(now.getDate() - 1);
+                                yesterday10PM.setHours(22, 0, 0, 0);
+                                const today10PM = new Date(now);
+                                today10PM.setHours(22, 0, 0, 0);
+
+                                if (
+                                    (today10PM.getTime() <= now.getTime() && res.likesCount.timestamp > today10PM.getTime()) || //If todays 10 Pm is Happened and Last Like was done after todays 10pm
+                                    (today10PM.getTime() > now.getTime() && res.likesCount.timestamp > yesterday10PM.getTime()) //If not todays 10 Pm is Happened and Last Like was done after yesterdays 10pm
+                                ) {
+                                    totalLikesCount = res.likesCount.value;
                                 }else{
-                                    const now = new Date();
-                                    const yesterday10PM = new Date(now);
-                                    yesterday10PM.setDate(now.getDate() - 1);
-                                    yesterday10PM.setHours(22, 0, 0, 0);
-                                    const today10PM = new Date(now);
-                                    today10PM.setHours(22, 0, 0, 0);
+                                    totalLikesCount = 0
+                                }
+                            };
+                            sendResponse({ likes: totalLikesCount });
+                        } catch (error) {
+                            sendResponse({ failed: true})
+                        }
+                    })();
+                    return true;
+                    break;
 
-                                    if (
-                                        (today10PM.getTime() <= now.getTime() && res.likesCount.timestamp > today10PM.getTime()) || //If todays 10 Pm is Happened and Last Like was done after todays 10pm
-                                        (today10PM.getTime() > now.getTime() && res.likesCount.timestamp > yesterday10PM.getTime()) //If not todays 10 Pm is Happened and Last Like was done after yesterdays 10pm
-                                    ) {
-                                        totalLikesCount = res.likesCount.value;
-                                    }else{
-                                        totalLikesCount = 0
-                                    }
-                                };
-                                sendResponse({ likes: totalLikesCount });
-                            } catch (error) {
-                                sendResponse({ failed: true})
-                            }
-                        })();
-                        return true;
-                        break;
-
-                    case "Updated Likes Limit":
-                        likesLimit = data.likesLimit
-                        chrome.storage.sync.set({
-                            likesLimit: {
-                                value: likesLimit
-                            },
-                        });
-                        break;
-                        
-                    case "give me likes limit":
-                        (async () => {
-                            try {
-                                let res = await chrome.storage.sync.get(["likesLimit"]);
-                                likesLimit = res.likesLimit.value;            
-                                sendResponse({likesLimit: likesLimit});
-                            } catch (error) {
-                                console.log("Nothing Found");
-                                
-                                sendResponse({ failed: true})
-                            }
-                        })();
-                        return true;
-                        break;
-                }
-                break;
-        }
+                case "Updated Likes Limit":
+                    likesLimit = data.likesLimit
+                    chrome.storage.sync.set({
+                        likesLimit: {
+                            value: likesLimit
+                        },
+                    });
+                    break;
+                    
+                case "give me likes limit":
+                    (async () => {
+                        try {
+                            let res = await chrome.storage.sync.get(["likesLimit"]);
+                            likesLimit = res.likesLimit.value;            
+                            sendResponse({likesLimit: likesLimit});
+                        } catch (error) {
+                            console.log("Nothing Found");
+                            
+                            sendResponse({ failed: true})
+                        }
+                    })();
+                    return true;
+                    break;
+            }
+            break;
     }
-);
+});
